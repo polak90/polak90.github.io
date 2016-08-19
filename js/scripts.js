1b921916be681cdd06be01074df01610e9584912
polak90 <kinfy.87@gmail.com>
2016-08-19 09:29:36 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
  function startTime() {
     var today = new Date();
-    var hr = today.getHours;
+    var hr = today.getHours();
     var min = today.getMinutes();
     var sec= today.getSeconds();
     ap = (hr < 12) ? "<span>AM</span" : "<span>PM</span";
